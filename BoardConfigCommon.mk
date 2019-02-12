ab2a763f37ba90cbe580aa3f32db04e7c5259e9a
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-12 18:12:00 +0100

    sm8250-common: Remove deprecated cmdline option
    * Firmware path is specified in ueventd.qcom

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,7 +98,6 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 printk.devkmsg=on
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 BOARD_KERNEL_IMAGE_NAME := Image
