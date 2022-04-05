ae7344c48218341f80cfe4543a4870601fa4f6bd
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    sm8250-common: Inherit from QTI FCM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,7 +76,7 @@ BOARD_ROOT_EXTRA_FOLDERS += \
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/configs/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest.xml
