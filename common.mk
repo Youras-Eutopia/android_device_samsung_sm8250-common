963881e4b930fe16ccbaeec836333b13db57a34d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    sm8250-common: Switch to QTI Memtrack AIDL HAL

--- common.mk
+++ common.mk
@@ -82,8 +82,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.kona \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
@@ -92,7 +90,6 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libtinyxml2 \
     libvulkan \
-    memtrack.kona \
     vendor.display.config@1.0 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@1.5 \
@@ -106,6 +103,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service
     AdvancedDisplay
 
 # Doze
